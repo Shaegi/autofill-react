72c335d857c4eecec3768a6968a6faec02d047f9
Marvin <marvin.semmelroth@gmail.com>
2020-11-14 19:17:10 +0100

    Read server url from dotenv

--- src/configureClient.ts
+++ src/configureClient.ts
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: process.env.REACT_APP_SERVER_URL,
   cache: new InMemoryCache()
 })
 
